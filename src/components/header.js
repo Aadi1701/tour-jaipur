98ddd31cf4225d12e2c03495d149f2754ae65fe5
Aadi1701 <aditya17cse@gmail.com>
2023-03-30 21:15:13 +0530

    added header

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function header(){
+  return(
+    <div className= "app-header">
+      <nav className= "nav-bar">
+        <span>item1</span>
+        <span>item2</span>
+        <span>item3</span>
+        <span>item4</span>
+        <span>item5</span>
+        <span>item6</span>
+      </nav>
+    </div>
+  );
+}
+
+export default header;
