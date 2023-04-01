29f41252ff8b6cdf472128db0c8ff80c21bee649
Aadi1701 <aditya17cse@gmail.com>
2023-04-01 14:07:39 +0530

    Section1 initiated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/Header.js';
 import HeaderC from './components/HeaderC.css';
+import Section1 from './components/Section1.js';
+import Section1C from './components/Section1C.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <Section1 />
     </div>
   );
 }
